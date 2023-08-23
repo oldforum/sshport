81a365d41e7dd5dff52fa240e56ef04742ec0d3d
oldforum <47047172+oldforum@users.noreply.github.com>
2023-08-23 16:44:25 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+# 检查sudo是否存在或者当前是否为root用户
+if ! command -v sudo &> /dev/null && [ "$EUID" -ne 0 ]; then
+    echo "sudo 未安装，并且当前不是root用户，请以root身份运行此脚本。"
+    exit 1
+fi
+
 # 随机生成2000-65535之间的端口
 NEW_PORT=$((RANDOM % (65535 - 2000 + 1) + 2000))
 
 # 更改SSH配置文件中的端口
-sudo sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
+if command -v sudo &> /dev/null; then
+    sudo sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
+    sudo systemctl restart sshd
 
-# 重启SSH服务
-sudo systemctl restart sshd
+    # 如果ufw防火墙已经安装和运行，更新规则
+    if sudo ufw status | grep -q "active"; then
+        sudo ufw allow $NEW_PORT/tcp
+    fi
+else
+    sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
+    systemctl restart sshd
 
-# 如果ufw防火墙已经安装和运行，更新规则
-if sudo ufw status | grep -q "active"; then
-    sudo ufw allow $NEW_PORT/tcp
+    # 如果ufw防火墙已经安装和运行，更新规则
+    if ufw status | grep -q "active"; then
+        ufw allow $NEW_PORT/tcp
+    fi
 fi
 
 # 提示用户新的端口号
