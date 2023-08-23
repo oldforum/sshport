04dd233a603f79c86b1cde537bc69196d7c71512
oldforum <47047172+oldforum@users.noreply.github.com>
2023-08-23 16:40:09 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-# 要求用户输入新的SSH端口
-read -p "请输入新的SSH端口: " new_port
+# 随机生成2000-65535之间的端口
+NEW_PORT=$((RANDOM % (65535 - 2000 + 1) + 2000))
 
-# 检查端口是否在指定范围内
-if [[ $new_port -lt 1024 || $new_port -gt 49151 ]]; then
-    echo "错误：端口值必须在1024-49151之间"
-    exit 1
-fi
-
-# 更改SSH配置文件中的端口值
-sudo sed -i "s/^Port .*/Port $new_port/" /etc/ssh/sshd_config
+# 更改SSH配置文件中的端口
+sudo sed -i "s/^Port .*/Port $NEW_PORT/" /etc/ssh/sshd_config
 
 # 重启SSH服务
 sudo systemctl restart sshd
 
 # 如果ufw防火墙已经安装和运行，更新规则
 if sudo ufw status | grep -q "active"; then
-    sudo ufw allow $new_port/tcp
+    sudo ufw allow $NEW_PORT/tcp
 fi
 
-# 提示用户更改已完成
-echo "SSH端口已更改为$new_port，请尝试使用新端口进行连接。"
-
+# 提示用户新的端口号
+echo "SSH端口已更改为$NEW_PORT，请使用此端口进行连接。"
