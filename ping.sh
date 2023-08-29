43869e15adafd0ab6bca58ec7d8365aa1ba437d4
oldforum <47047172+oldforum@users.noreply.github.com>
2023-08-30 02:18:59 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-ips=(
-  "91.229.133.25"
-  "85.237.68.236"
-)
+# 创建关联数组,key是IP,value是备注
+declare -A ips_desc
 
-for ip in ${ips[@]}
+ips_desc[91.229.133.25]="AK.HKLite"
+ips_desc[185.214.103.178]="AK.KRBGP"  
+ips_desc[103.232.213.62]="AK.JPPRO"
+ips_desc[181.214.136.81]="AK.JPLite"
+ips_desc[85.237.68.236]="AK.SGLite"
+ips_desc[108.165.254.235]="AK.LAXUS3"
+ips_desc[51.158.62.251]="AK.FRNAT"
+
+for ip in "${!ips_desc[@]}"
 do
   ping_resp=$(ping -c 3 $ip | tail -1)
   ping_time=$(echo $ping_resp | cut -d '/' -f 5 | cut -d '.' -f 1)
 
-  if [[ $ip == "91.229.133.25" ]]; then
-    echo "AK.HKLite, IP: $ip, Delay: $ping_time ms"  
-  elif [[ $ip == "85.237.68.236" ]]; then
-    echo "AK.SGLite, IP: $ip, Delay: $ping_time ms"
-  fi
+  echo "${ips_desc[$ip]}, IP: $ip, Delay: $ping_time ms"
 done
